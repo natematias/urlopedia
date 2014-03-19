8d166456de48c5495079b3db15877c7bd76fdca9
David Larochelle <github@larochelle.name>
2014-03-19 16:13:06 -0400

    Store data in MongoDB

--- routes/services.js
+++ routes/services.js
@@ -6,6 +6,11 @@ MediaCloud = require('./MediaCloudService');
 Herdict = require('./HerdictService');
 Describing = require('./DescribingAMService');
 
+
+var MongoClient = require('mongodb').MongoClient
+    , format = require('util').format;
+
+
 // validation method
 function validate_url(url){
   if(url!= null && url!=""){
@@ -32,6 +37,8 @@ exports.all = function (req, res) {
     return;
   }
 
+  
+
   herdict = Object.create(Herdict.HerdictService);
   wayback = Object.create(WayBack.WayBackService);
   mediacloud = Object.create(MediaCloud.MediaCloudService);
@@ -40,11 +47,38 @@ exports.all = function (req, res) {
   Promise.all( [
     url_service(url),
     herdict.fetch( url.replace( /^https?:\/\//, '' ) ),
-    describing.fetch(url),
-    mediacloud.fetch(url),
+    //describing.fetch(url),
+    //mediacloud.fetch(url),
     wayback.fetch(url)
   ] )
   .then( function( result ) {
+
+    console.log("storing result in mongo");
+
+    MongoClient.connect('mongodb://127.0.0.1:27017/urlopedia', function(err, db) {
+    if(err) throw err;
+
+    var collection = db.collection('all');
+//	collection.insert(result);
+
+	collection.insert({ 'url': url, 'responses': result}, { 'safe':true },  function(err, docs) {
+
+	    console.log(format("err = %s", err ) )
+      collection.count(function(err, count) {
+          console.log(format("err = %s, count = %s", err, count));
+      });
+
+      // Locate all the entries using find
+      collection.find().toArray(function(err, results) {
+        console.dir(results);
+        // Let's close the db
+        db.close();
+      });
+    });
+  })
+
+    console.log("returning result json");
+    
     res.json(result);
   } )
   .catch(function (e) {
